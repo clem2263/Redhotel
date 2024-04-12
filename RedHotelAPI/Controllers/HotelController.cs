a0ff40c7ce4ad352b0176258ef959c09b2c3b8f3
azops1 <axel.barrieux@ecole-hexagone.com>
2024-04-12 14:24:48 +0200

    HotelController

---
+++ RedHotelAPI/Controllers/HotelController.cs
@@ -0,0 +1,139 @@
+﻿using Dal;
+using DomainModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RedHotelAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HotelController : Controller
+    {
+        private readonly RedHotelContext context;
+
+        public HotelController(RedHotelContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet] // Accéder à l'hôtel
+        public IActionResult GetAll()
+        {
+            return Ok(this.context.Hotels.ToList());
+        }
+
+        [HttpGet("{id}")] // Lister un hôtel
+
+        public async Task<IActionResult> GetHotelById(int id)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var hotel = await context.Hotels.FindAsync(id);
+
+                if (hotel == null)
+                {
+                    Console.WriteLine($"[ HotelController ] - GetHotelById - the specified hotel was not found in the database, id : {id}");
+                    return NotFound();
+                }
+
+                return Ok(hotel);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"[ HotelController ] - GetAll - an error has occurred while retrieving all hotels : {e.Message}");
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpPost] // Créer un hôtel
+
+        public async Task<IActionResult> CreateHotel([FromBody] Hotel hotel)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                context.Hotels.Add(hotel);
+                await context.SaveChangesAsync();
+
+                return CreatedAtRoute("GetHotel", new { id = hotel.HotelID }, hotel);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"[ HotelController ] - GetAll - an error has occurred while retrieving all hotels : {e.Message}");
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpPut("{id}")] // Modifier un hôtel 
+
+        public async Task<IActionResult> UpdateHotel(int id, [FromBody] Hotel hotel)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                if (id != hotel.HotelID)
+                {
+                    return BadRequest();
+                }
+
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                context.Entry(hotel).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                await context.SaveChangesAsync();
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"[ HotelController ] - GetAll - an error has occurred while retrieving all hotels : {e.Message}");
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")] // Supprime un hôtel
+
+        public async Task<IActionResult> DeleteHotel(int id)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var hotel = await context.Hotels.FindAsync(id);
+
+                if (hotel == null)
+                {
+                    return NotFound();
+                }
+
+                context.Hotels.Remove(hotel);
+                await context.SaveChangesAsync();
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"[ CustomerController ] - GetAll - an error has occurred while retrieving all hotels : {e.Message}");
+                return StatusCode(500, e.Message);
+            }
+        }
+
+    }
+}
